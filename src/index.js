e74e3b557d6b5534112dd082c025d9f1051a7010
Steve Golden <sgolden2525@outlook.com>
2018-08-30 11:26:46 -0600

    Creating grid and box properties
    Adding box properties for the grid.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,32 @@ import './index.css';
 
 class Grid extends React.Component {
   render() {
+    const width = this.props.cols * 14;
+    var rowsArr = [];
+
+    var boxClass = "";
+    for (var i = 0; i < this.props.rows; i++) {
+      for (var j = 0; i < this.props.cols; j++) {
+        let boxId = i + "_" + j;
+
+        boxClass = this.props.gridFull[i][j] ? "box on" : "box off"
+        rowsArr.push(
+          <Box
+            boxClass={boxClass}
+            key={boxId}
+            boxId={boxId}
+            row={i}
+            col={j}
+            selectBox={this.props.selectBox}
+          />
+        )
+      }
+    }
+
     return (
-      <div>Grid</div>
+      <div className="grid" style={{width: width}}>
+        {{rowsArr}}
+      </div>
     );
   }
 }
@@ -30,6 +54,7 @@ class Main extends React.Component {
           grifFull={this.state.gridFull}
           rows={this.rows}
           cols={this.cols}
+          selectBox={this.selectBox}
         />
         <h2>Generations: { this.state.generation }</h2>
       </div>  
